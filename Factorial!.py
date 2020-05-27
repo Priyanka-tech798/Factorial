5c9b2c5a836927fa9efb3bc9a1c3bccc6e24bd58
Priyanka Agrawal <65527151+Priyanka-tech798@users.noreply.github.com>
2020-05-27 19:54:55 +0530

    Update Factorial!.py

--- Factorial!.py
+++ Factorial!.py
@@ -1,5 +1,23 @@
-n=int(input())
-f=1
-for i in range(1,n+1):
-    f=f*i
-print(f) 
+#Factorial of a given number by using Recursive
+def fact(n):
+    return 1 if (n==0 or n==1) else n*fact(n-1)
+num = 5
+print("Factorial of ",num,"is",fact(num))
+
+
+#Factorial of a number by using iterative
+def fact(n):
+    if n<0:
+        return 0
+    elif n==0 or n==1:
+        return 1
+    else:
+        f=1
+        while n>1:
+            f*=n
+            n-=1
+        return f    
+            
+num = 5
+print("Factorial of ",num,"is",fact(num))
+
